bbaa978b38559b656ef1d779ec634d80adc9851b
T1poDumaush1y <155962166+T1poDumaush1y@users.noreply.github.com>
2024-05-11 15:35:59 +0300

    1.2
    ракетки теперь на своих местах, все работает как надо, система очков, система проигрыша

--- Untitled-1.py
+++ Untitled-1.py
@@ -8,6 +8,8 @@ win_height = 700
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
+point1 = 0
+point2 = 0
 leftrocket = 'left_rocket.png'
 rightrocket = 'right_rocket.png'
 game = True
@@ -52,19 +54,21 @@ class Player(GameSprite):
  #метод "выстрел" (используем место игрока, чтобы создать там пулю)
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 5:
+        if keys[K_UP] and self.rect.y > 5:
            self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height:
+        if keys[K_DOWN] and self.rect.y < win_height:
             self.rect.y += self.speed
 
-left_rocket = Player(leftrocket, 30, 200, 100, 250, 4)
-right_rocket = Player(rightrocket, 30, 200, 100, 250, 4)
-ball = GameSprite('ball.png', 200, 200, 100, 100, 4)
+left_rocket = Player(leftrocket, 30, 300, 100, 150, 9)
+right_rocket = Player(rightrocket, 670, 300, 100, 150, 9)
+ball = GameSprite('ball.png', 300, 300, 50, 50, 19)
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 speed_x = 3
 speed_y = 3
-
-
 
 '''Игровой цикл'''
 
@@ -72,7 +76,7 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    
+        
     if finish != True:
         window.fill(back)
         left_rocket.update_l()
@@ -80,10 +84,37 @@ while game:
         ball.rect.x += speed_x
         ball.rect.y += speed_y
 
+        points1 = font.render('POINTS: '+ str(point1),  True, (0, 255, 0))
+        window.blit(points1, (200, 10))
+
+        points2 = font.render('POINTS: '+ str(point2),  True, (0, 255, 0))
+        window.blit(points2, (500, 10))
+
+        if sprite.collide_rect(left_rocket, ball):
+            speed_x *= -1
+            speed_y *= 1
+            point1 +=1
+
+        if sprite.collide_rect(right_rocket, ball):
+            speed_x *= -1
+            speed_y *= 1
+            point2 +=1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (300, 325))
+
+        if ball.rect.x > 700:
+            finish = True
+            window.blit(lose2, (300, 325))
+
     left_rocket.reset()
     right_rocket.reset()
     ball.reset()
-
-
+    
+    
     display.update()
     clock.tick(FPS)
