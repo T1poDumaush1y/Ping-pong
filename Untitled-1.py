45776f5a169bdf7e78bc37611e9a5239040e0508
T1poDumaush1y <155962166+T1poDumaush1y@users.noreply.github.com>
2024-05-18 15:34:51 +0300

    v 1.1

--- Untitled-1.py
+++ Untitled-1.py
@@ -12,11 +12,14 @@ point1 = 0
 point2 = 0
 leftrocket = 'left_rocket.png'
 rightrocket = 'right_rocket.png'
+scoreline = 'scoreline.png'
+right_will_get_point = False
+left_will_get_point = False
 game = True
 finish = False
 
 clock = time.Clock()
-FPS = 30
+FPS = 120
 
 '''Важные классы'''
 
@@ -49,19 +52,30 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
            self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height:
+        if keys[K_s] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
  #метод "выстрел" (используем место игрока, чтобы создать там пулю)
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
            self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height:
+        if keys[K_DOWN] and self.rect.y < win_height- 150:
             self.rect.y += self.speed
+
+class Scoreline(GameSprite):
+    def update_move(self):
+        self.rect.x += self.speed
+        if self.rect.x < win_width - 250:
+            self.speed *= -1
+        if self.rect.x > win_width - 750:
+            self.speed *= -1
+
 
 left_rocket = Player(leftrocket, 30, 300, 100, 150, 9)
 right_rocket = Player(rightrocket, 670, 300, 100, 150, 9)
 ball = GameSprite('ball.png', 300, 300, 50, 50, 19)
+scoreline_obj = Scoreline(scoreline, 300, 25, 200, 35, 3)
+
 
 font.init()
 font = font.Font(None, 35)
@@ -81,24 +95,37 @@ while game:
         window.fill(back)
         left_rocket.update_l()
         right_rocket.update_r()
+        scoreline_obj.update_move()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
 
         points1 = font.render('POINTS: '+ str(point1),  True, (0, 255, 0))
-        window.blit(points1, (200, 10))
+        window.blit(points1, (150, 10))
 
         points2 = font.render('POINTS: '+ str(point2),  True, (0, 255, 0))
-        window.blit(points2, (500, 10))
+        window.blit(points2, (550, 10))
 
         if sprite.collide_rect(left_rocket, ball):
             speed_x *= -1
             speed_y *= 1
-            point1 +=1
+            left_will_get_point == True
 
         if sprite.collide_rect(right_rocket, ball):
             speed_x *= -1
             speed_y *= 1
-            point2 +=1
+            right_will_get_point == True
+
+        if sprite.collide_rect(scoreline_obj, ball) and right_will_get_point == True:
+            speed_x *= -1 
+            point2 += 1
+        else:
+            right_will_get_point == False
+
+        if sprite.collide_rect(scoreline_obj, ball) and left_will_get_point == True:
+            speed_x *= -1
+            point1 += 1
+        else:
+            left_will_get_point == False
 
         if ball.rect.y > win_height-50 or ball.rect.y < 0:
             speed_y *= -1
@@ -114,6 +141,7 @@ while game:
     left_rocket.reset()
     right_rocket.reset()
     ball.reset()
+    scoreline_obj.reset()
     
     
     display.update()
